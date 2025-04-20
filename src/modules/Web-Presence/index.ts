f393f201537e308c82f7dd7a99b447750d71c6fd
Armando Peralta <armandoperalta@webdesignnodes.com>
2025-04-20 02:34:48 -0600

    Build: Web Presence Module > Users Methods Update.

--- src/modules/Web-Presence/index.ts
+++ src/modules/Web-Presence/index.ts
@@ -66,11 +66,154 @@ class Web_Presence {
 
     }
 
+    private login_Email_Password_Account(email: string, password: string, keep_session_open: boolean): Promise<{ authenticated: boolean, message: string, session_token?: string, refresh_token?: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.login_Email_Password_Account({ email: email.toLowerCase().trim(), password: password.trim(), keep_session_open: keep_session_open }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to login into the User Email-Password Account");
+                throw Error(`An error has ocurred while trying to login into the User Email-Password Account: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    private refresh_Email_Password_Session_Session_Token(session_token: string, refresh_token: string): Promise<{ new_Token: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.refresh_Email_Password_Account({ session_token: session_token, refresh_token: refresh_token }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to refresh session token");
+                throw Error(`An error has ocurred while trying to refresh session token: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    private logout(session_token: string): Promise<{ closed: boolean, message: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.logout({ session_token: session_token }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to logout");
+                throw Error(`An error has ocurred while trying to logout: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    private reset_Email_Password_Account_Password(email: string): Promise<{ reset_email_sended: boolean }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.reset_Email_Password_Account_Password({ email: email }).then((fetch_response) => {
+
+                return resolve(fetch_response)
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to send account password reset email");
+                throw Error(`An error has ocurred while trying to send account password reset email: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    private update_Email_Password_Account_Password(account_ID: string, new_Password: string): Promise<{ updated: boolean, message: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.update_Email_Password_Account_Password({ account_ID: account_ID, new_Password: new_Password }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to update the account password");
+                throw Error(`An error has ocurred while trying to update the account password: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    public get_Account_Data(account_ID: string, data: Record<string, number>): Promise<{ found: boolean, data: Record<string, any>, message: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.get_Account_Data({ account_ID, data }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to get the account data");
+                throw Error(`An error has ocurred while trying to get the account data: \n${err}`);
+
+            })
+
+        })
+
+    }
+
+    public delete_Account(account_ID: string): Promise<{ deleted: boolean, message: string }> {
+
+        return new Promise((resolve, reject) => {
+
+            this.API_Fetcher.Web_Presence.User.detele_Account({ account_ID }).then((fetch_response) => {
+
+                return resolve(fetch_response);
+
+            }).catch((err) => {
+
+                reject("An error has ocurred while trying to delete the account");
+                throw Error(`An error has ocurred while trying to delete the account: \n${err}`);
+
+            })
+
+        })
+
+    }
+
     public User = {
 
         create_Email_Password_Account: (email: string, password: string, account_custome_attributes?: unknown) => this.create_Email_Password_Account(email, password, account_custome_attributes),
+        login_Email_Password_Account: (email: string, password: string, keep_session_open: boolean) => this.login_Email_Password_Account(email, password, keep_session_open),
+        refresh_Email_Password_Session_Session_Token: (session_token: string, refresh_token: string) => this.refresh_Email_Password_Session_Session_Token(session_token, refresh_token),
+        logout: (session_token: string) => this.logout(session_token),
+        update_Account: (email: string, password: string, account_custome_attributes?: unknown) => this.create_Email_Password_Account(email, password, account_custome_attributes),
+        reset_Email_Password_Account_Password: (account_Email: string) => this.reset_Email_Password_Account_Password(account_Email),
+        update_Email_Password_Account_Password: (account_ID: string, new_Password: string) => this.update_Email_Password_Account_Password(account_ID, new_Password),
+        get_Account_Data: (account_ID: string, data: Record<string, number>) => this.get_Account_Data(account_ID, data),
+        delete_Account: (account_ID: string) => this.delete_Account(account_ID)
 
     }
+
+    // * Web Presence Database Methods
 
     private find_Database_Documents(database_name: string, filter: Record<string, any>): Promise<{ acknowledged: boolean }> {
 
