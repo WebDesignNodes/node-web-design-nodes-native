d3bcc2c857eb1bd51e7ab0abcd481b3e1a76ac98
Armando Peralta <armandoperalta@webdesignnodes.com>
2025-04-20 02:32:57 -0600

    Build: User Module Response Update.

--- src/libs/API/response-interface/web-presence/user.response.ts
+++ src/libs/API/response-interface/web-presence/user.response.ts
@@ -1,2 +1,18 @@
 
-export interface create_Email_Password_Account_Response { created: boolean, account_ID: string | null, message: string }
+export interface create_Email_Password_Account_Response { created: boolean, account_ID: string | null, message: string };
+
+export interface login_Email_Password_Account_Response { authenticated: boolean, session_token?: string, refresh_token?: string, message: string };
+
+export interface refresh_Email_Password_Account_Session_Session_Token_Response { new_Token: string, message: string };
+
+export interface logout_Response { closed: boolean, message: string };
+
+export interface update_Account { updated: boolean, message: string };
+
+export interface reset_Email_Password_Account_Password_Response { reset_email_sended: boolean, message: string };
+
+export interface update_Email_Password_Account_Password_Response { updated: boolean, message: string };
+
+export interface get_Account_Data_Response { found: boolean, data: Record<string, any>, message: string };
+
+export interface delete_Account_Response { deleted: boolean, message: string };
