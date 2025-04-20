ea524ddb3258ecc6fe9ae3a65e4822030d0a4d43
Armando Peralta <armandoperalta@webdesignnodes.com>
2025-04-20 02:36:15 -0600

    Build: Web Presence API Fetcher Update.

--- src/libs/API/Web-Presence-API-Fetcher.ts
+++ src/libs/API/Web-Presence-API-Fetcher.ts
@@ -6,12 +6,12 @@ import { Database_Routes } from "./routes/web-presence/database.routes";
 
 // * Payload Interfaces Required
 
-import { create_Email_Password_Account_Payload } from "./payload-interface/web-presence/user.payload";
+import * as Account_Payload from "./payload-interface/web-presence/user.payload";
 import { find_Database_Documents_Payload } from "./payload-interface/web-presence/database.payload";
 
 // * Response Interfaces Required
 
-import { create_Email_Password_Account_Response } from "./response-interface/web-presence/user.response";
+import * as Account_Response from "./response-interface/web-presence/user.response";
 import { find_Database_Documents_Response } from "./response-interface/web-presence/database.response";
 
 // * Class Exported
@@ -59,7 +59,15 @@ class Web_Presence_API_Fetcher {
     public Web_Presence = {
 
         User: {
-            create_Email_Password_Account: (payload: create_Email_Password_Account_Payload): Promise<create_Email_Password_Account_Response> => this.send_Request(User_Manager_Routes.create_email_password_account, "POST", payload)
+            create_Email_Password_Account: (payload: Account_Payload.create_Email_Password_Account_Payload): Promise<Account_Response.create_Email_Password_Account_Response> => this.send_Request(User_Manager_Routes.create_email_password_account, "POST", payload),
+            login_Email_Password_Account: (payload: Account_Payload.login_Email_Password_Account_Payload): Promise<Account_Response.login_Email_Password_Account_Response> => this.send_Request(User_Manager_Routes.login_email_password_account, "POST", payload),
+            refresh_Email_Password_Account: (payload: Account_Payload.refresh_Email_Password_Account_Session_Session_Token_Payload): Promise<Account_Response.refresh_Email_Password_Account_Session_Session_Token_Response> => this.send_Request(User_Manager_Routes.refresh_session_token, "POST", payload),
+            logout: (payload: Account_Payload.logout_Payload): Promise<Account_Response.logout_Response> => this.send_Request(User_Manager_Routes.logout, "POST", payload),
+            update_Account: (payload: Account_Payload.update_Account_Payload): Promise<Account_Response.update_Account> => this.send_Request(User_Manager_Routes.update_account, "POST", payload),
+            reset_Email_Password_Account_Password: (payload: Account_Payload.reset_Email_Password_Account_Password_Payload): Promise<Account_Response.reset_Email_Password_Account_Password_Response> => this.send_Request(User_Manager_Routes.reset_email_password_account_password, "POST", payload),
+            update_Email_Password_Account_Password: (payload: Account_Payload.update_Email_Password_Account_Password_Payload): Promise<Account_Response.update_Email_Password_Account_Password_Response> => this.send_Request(User_Manager_Routes.update_email_password_account_password, "POST", payload),
+            get_Account_Data: (payload: Account_Payload.get_Account_Data_Payload): Promise<Account_Response.get_Account_Data_Response> => this.send_Request(User_Manager_Routes.get_account_data, "POST", payload),
+            detele_Account: (payload: Account_Payload.delete_Account_Payload): Promise<Account_Response.delete_Account_Response> => this.send_Request(User_Manager_Routes.delete_account, "POST", payload)
         },
 
         Database: {
