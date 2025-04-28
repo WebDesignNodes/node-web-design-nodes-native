ef3c27385e7e146e6fa921d366e685ca7762c77d
Armando Peralta <armandoperalta@webdesignnodes.com>
2025-04-28 10:10:52 -0600

    Build: Web Presence > Database Unit Test implementation.

--- tests/Database/Web_Presence-Database-Find.test.ts
+++ tests/Database/Web_Presence-Database-Find.test.ts
@@ -1,14 +1,148 @@
 
-import Web_Design_Nodes from "../src"
+// * Dependencies Required
 
-test("Load Web Presence Database Content", async () => {
+import Web_Design_Nodes from "../../src";
 
-    const web_Presence_Connection = Web_Design_Nodes.Web_Presence.connect({ project_id: "web-design-nodes-news-feed", project_token: "b4add9fd4a04b478b1a70da2d8ebd33905efad0ad72fc5d686c62bc0b9f2cfd0" })
+describe("Web Presence Database Workflow", () => {
 
-    const search_Result = await web_Presence_Connection.Database.find("domains", {});
+    const web_Presence_Connection = Web_Design_Nodes.Web_Presence.connect({ project_id: "joseirasso-network", project_token: "9558ef3b61152c4752de4cbc296426427b561db6119cf964e1dac8eb3bd31845" })
 
-    console.log(search_Result);
+    test("Save One document at the Web Presence Database", async () => {
 
-    expect(search_Result)
+        const testing_document = {
+            title: "Articulo de prueba",
+            content: ["Esto representa un parrafo del articulo", "Esto representa otro parrafo del articulo"],
+            releaseDate: Date.now()
+        }
+
+        const insert_Result = await web_Presence_Connection.Database.save_One_Document("testing", testing_document);
+
+        console.log(insert_Result);
+
+        expect(insert_Result.acknowledged === true);
+
+    });
+
+    test("Save Many documents at the Web Presence Database", async () => {
+
+        const testing_document = {
+            title: "Articulo de prueba2",
+            content: ["Esto representa un parrafo del articulos", "Esto representa otro parrafo del articulos"],
+            releaseDate: Date.now()
+        }
+
+        const testing_document2 = {
+            title: "Articulo de prueba3",
+            content: ["Esto representa un parrafo del articulo", "Esto representa otro parrafo del articulo"],
+            releaseDate: Date.now()
+        }
+
+        const search_Result = await web_Presence_Connection.Database.save_Many_Documents("testing", [testing_document, testing_document2]);
+
+        console.log(search_Result);
+
+        expect(search_Result);
+
+    });
+
+    test("Find One Web Presence Database Content", async () => {
+
+        const filter = {
+            content: ["Esto representa un parrafo del articulo", "Esto representa otro parrafo del articulo"],
+        }
+
+        const projection = {
+            title: 1
+        }
+
+        const search_Result = await web_Presence_Connection.Database.find_One_Document("testing", filter, projection);
+
+        console.log(search_Result);
+
+        expect(search_Result);
+
+    });
+
+    test("Find Many Web Presence Database Content", async () => {
+
+        const filter = {
+            content: ["Esto representa un parrafo del articulo", "Esto representa otro parrafo del articulo"],
+        }
+
+        const projection = {
+            title: 1
+        }
+
+        const search_Result = await web_Presence_Connection.Database.find_Many_Documents("testing", filter, projection);
+
+        console.log(search_Result);
+
+        expect(search_Result);
+
+    });
+
+    test("Update One Web Presence Database Document", async () => {
+
+        const filter = {
+            title: "Articulo de prueba",
+        }
+
+        const update = {
+            $set: { title: "Articulo de prueba - one updated" }
+        }
+
+        const search_Result = await web_Presence_Connection.Database.update_One_Document("testing", filter, update, false);
+
+        console.log(search_Result);
+
+        expect(search_Result);
+
+    });
+
+    test("Update Many Web Presence Database Document", async () => {
+
+        const filter = {
+            content: ["Esto representa un parrafo del articulos", "Esto representa otro parrafo del articulos"],
+        }
+
+        const update = {
+            $set: { title: "Acticulo de pruebas - many updated" }
+        }
+
+        const search_Result = await web_Presence_Connection.Database.update_Many_Documents("testing", filter, update, false);
+
+        console.log(search_Result);
+
+        expect(search_Result);
+
+    });
+
+    // test("Delete One Web Presence Database Document", async () => {
+
+    //     const filter = {
+    //         title: "Articulo de prueba",
+    //     }
+
+    //     const search_Result = await web_Presence_Connection.Database.delete_One_Document("testing", filter);
+
+    //     console.log(search_Result);
+
+    //     expect(search_Result);
+
+    // });
+
+    // test("Delete Many Web Presence Database Documents", async () => {
+
+    //     const filter = {
+    //         content: ["Esto representa un parrafo del articulo", "Esto representa otro parrafo del articulo"],
+    //     }
+
+    //     const search_Result = await web_Presence_Connection.Database.delete_Many_Documents("testing", filter);
+
+    //     console.log(search_Result);
+
+    //     expect(search_Result);
+
+    // });
 
 })
